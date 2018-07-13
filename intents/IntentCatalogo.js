904133e73cf8ddd5784cedd11d2b572a93dff517
JoseArroyo <jarroyo@sapia.com.pe>
2018-07-13 17:43:37 -0500

    Agregando HeroCards para el catalogo

--- intents/IntentCatalogo.js
+++ intents/IntentCatalogo.js
@@ -1,7 +1,30 @@
 module.exports = function(bot){
+	var builder = require('botbuilder')
+	var servicioCatalogo = require.main.require('./services/ServiceCatalogo.js')
+	function getCardsAttachmentsCatalogo(session){
+		var productos = servicioCatalogo.getCatalogo()
+		var cards = []
+		productos.forEach(function(producto){
+			var card = new builder.HeroCard(session)
+				.title(producto.nombre)
+				.subtitle(producto.categoria)
+				.text(`Precio oferta: ${producto.precioOferta} Precio regular: ${producto.precioRegular}`)
+				.images([
+					builder.CardImage.create(session, producto.imagen)
+				])
+			cards.push(card)
+		})
+		return cards
+	}
+
 	bot.dialog('Catalogo', [
 		function(session){
-			session.endDialog("cargando catalogo de productos...")
+			session.send("cargando catalogo de productos...")
+			var cards = getCardsAttachmentsCatalogo(session)
+			var reply = new builder.Message(session)
+				.attachmentLayout(builder.AttachmentLayout.carousel)
+				.attachments(cards)
+			session.send(reply)
 		}
 	])	
 }
---
+++ services/ServiceCatalogo.js
@@ -0,0 +1,34 @@
+module.exports = {
+	getCatalogo: function (){
+		return [
+			{
+				categoria: "Terma",
+				nombre: "TERMA AQUAMAXX 5.5 L",
+				precioOferta: "Desde S/13.75 mensual - plazo 5 años",
+				precioRegular: "S/469.00",
+				imagen: "https://www.calidda.com.pe/ofivirtual/PublishingImages/Catálogo/terma55.png"
+			},
+			{
+				categoria: "Terma",
+				nombre: "TERMA AQUAMAXX 10 L TIRO FORZADO",
+				precioOferta: "Desde S/26.36 mensual - plazo 5 años",
+				precioRegular: "S/899.00",
+				imagen: "https://www.calidda.com.pe/ofivirtual/PublishingImages/Catálogo/terma10.png"
+			},
+			{
+				categoria: "Terma",
+				nombre: "TERMA A GAS DE PASO CONTINUO 5.5 LT",
+				precioOferta: "Desde S/18.10 mensual - plazo 5 años",
+				precioRegular: "S/617.40",
+				imagen: "https://www.calidda.com.pe/ofivirtual/PublishingImages/Catálogo/termasole55.png"
+			},
+			{
+				categoria: "Terma",
+				nombre: "TERMA A GAS DE PASO CONTINUO 10 L TIRO FORZADO",
+				precioOferta: "Desde S/37.60 mensual - plazo 5 años",
+				precioRegular: "S/1,282.40",
+				imagen: "https://www.calidda.com.pe/ofivirtual/PublishingImages/Catálogo/termasole10.png"
+			}
+		]
+	}
+}
