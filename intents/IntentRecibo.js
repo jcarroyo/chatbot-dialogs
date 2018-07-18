bd44611b8b0620e161ed9ccd04f8f989612f5357
JoseArroyo <jarroyo@sapia.com.pe>
2018-07-18 16:01:53 -0500

    changes to support attachment

--- intents/IntentRecibo.js
+++ intents/IntentRecibo.js
@@ -8,12 +8,10 @@ module.exports = function(bot){
 			var dni = results.response;
 			session.send("Dame unos minutos para buscar tu recibo")
 			session.sendTyping();
-			setTimeout(function(){
-				var reply = new builder.Message()
-					.setText(session, "Recibo de julio 2018")
-					.addAttachment({ fallbackText: "Gracias por nada", contentType: 'image/jpeg', contentUrl: "https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX2729462.jpg" })
-				session.send(reply)
-			}, 3500)
+			var reply = new builder.Message()
+				.setText(session, "Recibo de julio 2018")
+				.addAttachment({ fallbackText: "Gracias por nada", contentType: 'image/jpeg', contentUrl: "https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX2729462.jpg" })
+			session.send(reply)			
 		}
 	])	
 }
