95472ac39a5f97f3ce6385eea292df8b4cf9efb2
JoseArroyo <jarroyo@sapia.com.pe>
2018-07-20 16:24:38 -0500

    dialogo de facturacion

--- intents/IntentSolicitarProducto.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(bot){
-	var builder = require('botbuilder')
-	bot.dialog('Solicitar Producto', [
-		function(session){
-			session.send("solicituar producto")		
-		}
-	])	
-}
